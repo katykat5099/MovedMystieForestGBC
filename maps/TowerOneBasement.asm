f31b9d471caa4b3865396ff8d364f3574a398a08
Kaitlyn <katykat5099@gmail.com>
2023-08-17 01:55:24 -0500

    [STORY/MAIN] Introductory Phase Part 3a
    + Created the scripts for puzzle in the basement

--- maps/TowerOneBasement.asm
+++ maps/TowerOneBasement.asm
@@ -5,6 +5,38 @@ TowerOneBasement_MapScripts:
 
 	def_callbacks
 
+SecretButtonPress:
+    setevent EVENT_SECRET_WARP_IN_BASEMENT
+    playsound SFX_ENTER_DOOR
+    end
+
+SecretWarpTile:
+    checkevent EVENT_SECRET_WARP_IN_BASEMENT
+    iffalse .somethingDoesntSeemRight
+    warp TOWER_ONE_BASEMENT, 0, 15
+    playsound SFX_WARP_TO
+    end
+
+.somethingDoesntSeemRight
+    opentext
+    writetext SomethingDoesntSeemRightAboutThisMaze
+    promptbutton
+    closetext
+    end
+
+
+WarpTileBack:
+    warp TOWER_ONE_BASEMENT, 16, 4
+    playsound SFX_WARP_FROM
+    end
+
+;TEXT BELOW
+SomethingDoesntSeemRightAboutThisMaze:
+    text "Something doesn't"
+    line "seem right about"
+    cont "this maze…"
+    done
+
 TowerOneBasement_MapEvents:
 	db 0, 0 ; filler
 
@@ -15,6 +47,9 @@ TowerOneBasement_MapEvents:
 	warp_event  18,  9, TOWER_ONE_INDOOR_F1, 6
 
 	def_coord_events
+	coord_event 12, 13, SCENE_DEFAULT, SecretButtonPress
+	coord_event 16,  4, SCENE_DEFAULT, SecretWarpTile
+	coord_event  0, 15, SCENE_DEFAULT, WarpTileBack
 
 	def_bg_events
 
